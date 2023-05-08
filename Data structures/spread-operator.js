f2bafcd10d114ff20c46e9695ef1d192a9345bb6
berylcl <gakiiberyl@gmail.com>
2023-05-08 21:31:24 +0100

    Add spread operators

--- Data structures/spread-operator.js
+++ Data structures/spread-operator.js
@@ -55,3 +55,35 @@ console.log(mergeMenu)
 const str = 'Gakii'
 const letters = [...str, '', 'b.']
 console.log(letters)
+// NEW OBJ
+const newRestaurant = {...restaurant, foundedIn: 2032, founder: 'Gakii'}
+console.log(newRestaurant)
+const restaurantCopy = {...restaurant}
+restaurantCopy.name = 'Ristoranta roma'
+console.log(restaurant.name)
+
+//REST PATTERN AND PARAMETERS  REST PACKS ELEMENT INTO AN ARRAY
+const arr1 = [1,2, ...[3,4]]
+const[a,b,...others] = [1,2,3,4,5]
+console.log(a,b,others)
+//examples
+//SPREAD AN ARRAY OVER ANOTHER ARRAY
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+const spreadArr = [...array1,...array2]
+console.log(spreadArr)
+
+//MERGE TWO OBJECTS TO ONE
+const obj1 = { name: "Beryl", age: 2037 - 1999, interests: "movies" };;
+const obj2 = {name: 'ivy', age: 2037-1996, interests: 'having fun'}
+const mergedObjects = {...obj1,...obj2}
+console.log(mergedObjects)
+const object1 = { a: 1, b: 2 };
+const object2 = { c: 3, d: 4 };
+const mergedObject = { ...object1, ...object2 };
+console.log(mergedObject); // Output: { a: 1, b: 2, c: 3, d: 4 }
+//Pass an array as individual arguments to a function
+const add = [1,3,5,9,4]
+const sum = (a,b,c,d,e) => a+b+c+d+e
+const addition =sum(...add)
+console.log(addition)
